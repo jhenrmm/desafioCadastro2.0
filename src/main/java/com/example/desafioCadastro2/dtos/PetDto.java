38bebbfd6cbe3cda4d6ce853f24368ac277a5224
João <jhenrmm@gmail.com>
2025-10-16 20:06:49 -0300

    refactor: altered the name of the DTO PetRecordsDto to PetDto

--- src/main/java/com/example/desafioCadastro2/dtos/PetDto.java
+++ src/main/java/com/example/desafioCadastro2/dtos/PetDto.java
@@ -5,5 +5,5 @@ import com.example.desafioCadastro2.models.Tipo;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 
-public record PetsRecordsDto(@NotBlank String name, @NotNull Tipo tipo, @NotNull Sexo sexo, @NotBlank String endereco, @NotNull float idade,@NotNull float peso,@NotBlank String raca) {
+public record PetDto(@NotBlank String name, @NotNull Tipo tipo, @NotNull Sexo sexo, @NotBlank String endereco, @NotNull float idade, @NotNull float peso, @NotBlank String raca) {
 }
