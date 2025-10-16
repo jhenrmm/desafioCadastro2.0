03fc93aeeca9b9e76ddba7c0353bde47dbab607c
João <jhenrmm@gmail.com>
2025-10-16 20:00:46 -0300

    feat: added custom Exception

---
+++ src/main/java/com/example/desafioCadastro2/validations/ValidacaoException.java
@@ -0,0 +1,7 @@
+package com.example.desafioCadastro2.validations;
+
+public class ValidacaoException extends RuntimeException {
+    public ValidacaoException(String message) {
+        super(message);
+    }
+}
