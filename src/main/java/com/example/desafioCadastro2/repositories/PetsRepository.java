ddd3cbcfbbd93101fe84da23206a34e7bb831bd6
João <jhenrmm@gmail.com>
2025-10-16 20:03:48 -0300

    refactor: changed the type of id from UUID to Long

--- src/main/java/com/example/desafioCadastro2/repositories/PetsRepository.java
+++ src/main/java/com/example/desafioCadastro2/repositories/PetsRepository.java
@@ -1,11 +1,11 @@
 package com.example.desafioCadastro2.repositories;
 
-import com.example.desafioCadastro2.models.PetsModel;
+import com.example.desafioCadastro2.models.Pet;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 
 @Repository
-public interface PetsRepository extends JpaRepository<PetsModel, UUID> {
+public interface PetsRepository extends JpaRepository<Pet, Long> {
 }
