0f2c009c75994b6ba6c20da7aac648f128f79a33
João <jhenrmm@gmail.com>
2025-10-16 20:05:21 -0300

    refactor: optimized the controller and all the methods following the best practices

--- src/main/java/com/example/desafioCadastro2/controllers/PetsControllers.java
+++ src/main/java/com/example/desafioCadastro2/controllers/PetsControllers.java
@@ -1,51 +1,41 @@
 package com.example.desafioCadastro2.controllers;
 
-import com.example.desafioCadastro2.dtos.PetsRecordsDto;
-import com.example.desafioCadastro2.models.PetsModel;
+import com.example.desafioCadastro2.dtos.PetDto;
+import com.example.desafioCadastro2.models.Pet;
 import com.example.desafioCadastro2.models.Tipo;
-import com.example.desafioCadastro2.repositories.PetsRepository;
+import com.example.desafioCadastro2.service.PetsService;
 import jakarta.validation.Valid;
-import lombok.extern.log4j.Log4j;
-import org.springframework.beans.BeanUtils;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-import java.util.Optional;
-import java.util.UUID;
-import java.util.stream.Collectors;
-
 
 @RestController
 public class PetsControllers {
 
-    @Autowired
-    PetsRepository petsRepository;
+    private PetsService petsService;
 
     @PostMapping("/pets")
-    public ResponseEntity<PetsModel> savePets(@RequestBody @Valid PetsRecordsDto petsRecordsDto){
-        var petsModel = new PetsModel();
-        BeanUtils.copyProperties(petsRecordsDto, petsModel);
-        return ResponseEntity.status(HttpStatus.CREATED).body(petsRepository.save(petsModel));
+    public ResponseEntity<Pet> savePets(@RequestBody @Valid PetDto petDto){
+        petsService.save(petDto);
+        return ResponseEntity.status(HttpStatus.CREATED).build();
     }
+
     @GetMapping("/pets")
-    public ResponseEntity<List<PetsModel>> getAllPets(){
-        List<PetsModel> petsList = petsRepository.findAll();
+    public ResponseEntity<List<Pet>> listAllPets(){
+        List<Pet> petsList = petsService.listarTodosPets();
         return ResponseEntity.status(HttpStatus.OK).body(petsList);
     }
 
     @GetMapping("/pets/{id}")
-    public ResponseEntity<Object> getOnePet(@PathVariable(value = "id") UUID id){
-        Optional<PetsModel> petsO = petsRepository.findById(id);
-        if (petsO.isEmpty()){
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Pet not found.");
-        }
-        return ResponseEntity.status(HttpStatus.OK).body(petsO.get());
+    public ResponseEntity<Object> listPetById(@PathVariable(value = "id") Long id){
+        Pet pet = petsService.listarPetPorId(id);
+        return ResponseEntity.status(HttpStatus.OK).body(pet);
     }
+
     @GetMapping("/petsCriterio")
-    public ResponseEntity<List<PetsModel>> buscarPetsCriterio(
+    public ResponseEntity<List<Pet>> listPetByCriterio(
             @RequestParam Tipo tipo,
             @RequestParam(required = false) String nome,
             @RequestParam(required = false) String sexo,
@@ -54,41 +44,20 @@ public class PetsControllers {
             @RequestParam(required = false) String raca,
             @RequestParam(required = false) String endereco
     ) {
-        List<PetsModel> pets = buscarPets(tipo, nome, sexo, idade, peso, raca, endereco);
+        List<Pet> pets = petsService.buscarPets(tipo, nome, sexo, idade, peso, raca, endereco);
         return ResponseEntity.status(HttpStatus.OK).body(pets);
     }
 
-
     @PutMapping("/pets/{id}")
-    public ResponseEntity<Object> updatePetById(@PathVariable(value = "id") UUID id, PetsRecordsDto petsRecordsDto){
-        Optional<PetsModel> petsO = petsRepository.findById(id);
-        if (petsO.isEmpty()){
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Pet not found");
-        }
-        var petsModel = petsO.get();
-        BeanUtils.copyProperties(petsRecordsDto, petsModel);
-        return ResponseEntity.status(HttpStatus.OK).body(petsRepository.save(petsModel));
+    public ResponseEntity<Object> updatePetById(@PathVariable(value = "id") Long id, PetDto petDto){
+        Pet pet = petsService.atualizarPetPorId(id, petDto);
+        return ResponseEntity.status(HttpStatus.OK).body(pet);
     }
 
     @DeleteMapping("/pets/{id}")
-    public ResponseEntity<Object> deletePet(@PathVariable(value = "id") UUID id){
-        Optional<PetsModel> petsO = petsRepository.findById(id);
-        if (petsO.isEmpty()){
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Pet not Found.");
-        }
-        petsRepository.delete(petsO.get());
-        return ResponseEntity.status(HttpStatus.OK).body("Pet deleted successfully");
-    }
-    public List<PetsModel> buscarPets(Tipo tipo, String nome, String sexo, Float idade, Float peso, String raca, String endereco) {
-        return petsRepository.findAll().stream()
-                .filter(p -> p.getTipo().equals(tipo))
-                .filter(p -> nome == null || p.getName().toLowerCase().contains(nome.toLowerCase()))
-                .filter(p -> sexo == null || p.getSexo().name().equalsIgnoreCase(sexo))
-                .filter(p -> idade == null || p.getIdade().equals(idade))
-                .filter(p -> peso == null || p.getPeso().equals(peso))
-                .filter(p -> raca == null || p.getRaca().toLowerCase().contains(raca.toLowerCase()))
-                .filter(p -> endereco == null || p.getEndereco().toLowerCase().contains(endereco.toLowerCase()))
-                .collect(Collectors.toList());
+    public ResponseEntity<Object> deletePet(@PathVariable(value = "id") Long id){
+        petsService.deltarPetPorId(id);
+        return ResponseEntity.noContent().build();
     }
 
 }
