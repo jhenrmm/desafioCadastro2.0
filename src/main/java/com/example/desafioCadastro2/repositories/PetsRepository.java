a26539f5b1750b318c1b6aa4110e5c2e50502ab5
João <jhenrmm@gmail.com>
2025-10-16 20:16:27 -0300

    style: removed unused imports

--- src/main/java/com/example/desafioCadastro2/repositories/PetsRepository.java
+++ src/main/java/com/example/desafioCadastro2/repositories/PetsRepository.java
@@ -4,8 +4,6 @@ import com.example.desafioCadastro2.models.Pet;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.UUID;
-
 @Repository
 public interface PetsRepository extends JpaRepository<Pet, Long> {
 }
